d9a2097b20c90a8ca9ee3d87c463228dc79c08b0
cmp75 <118092017+cmp75@users.noreply.github.com>
2023-01-22 22:20:41 -0500

    updated files

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+  {
+    title: "Commonly used data typs Do Not Include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition is an if / else statement is enclosed with ______",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parenthesis"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ______",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+  },
+  {
+    title: "String values must be enclosed within _____ when being assigned to variables:",
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes"
+  },
+  {
+    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log"
+  },
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,142 @@
-// Comments show the vanilla JavaScript equivalent statements
+var questionsDisplay = document.querySelector("#questions");
+var timerDisplay = document.querySelector("#time"); 
+var answerOptions = document.querySelector("#answer-options"); 
+var submitButton = document.querySelector("#submit"); 
+var backButton = document.getElementById("go-back-button");
+var startButton = document.querySelector("#btn-start");
+var userInitialsInput = document.querySelector("#user-initials");
+var rightWrong = document.querySelector("#right-wrong");
 
-// var rootEl = document.getElementById("root");
-var rootEl = $('#root');
+var currentQuestionIndex = 0;
+var time = questions.length * 10; 
+var timerId; 
 
-// var titleEl = document.createElement("h1");
-var titleEl = $('<h1>');
-
-// titleEl.textContent = "Hello friends";
-titleEl.text('Hello friends');
-
-// titleEl.className = 'fancy';
-titleEl.attr('class', 'fancy');
-
-// titleEl.classList.add('p-5') - (`p-5` is for padding)
-titleEl.addClass('p-5');
-
-// titleEl.style.border = "rgb(122, 242, 242) 3px solid";
-titleEl.css('border', 'rgb(122, 242, 242) 5px solid');
-
-// rootEl.appendChild(titleEl);
-rootEl.append(titleEl);
-
-// titleEl.append("Welcome to jQuery");
-rootEl.append('<h2>With jQuery we can:</h2>');
-
-var abilities = [
-  'Select',
-  'Create',
-  'Style',
-  'Animate',
-  'Traverse',
-  'Event Listen',
-  'much more',
-];
-
-for (var i = 0; i < abilities.length; i++) {
-  // Create a new `<div>` for each ability and its text content
-  var abilityEl = $('<li>');
-
-  // abilityEl.textContent = abilities[i];
-  abilityEl.text(abilities[i]);
-
-  // `my-3` class adds margin on top and bottom
-  abilityEl.addClass('my-3');
-
-  // Alternatively
-  // var abilityEl = $("<div>" + abilities[i] + "</div>");
-
-  // Add this new `<div>` to the abilities `<div>` container element.
-  rootEl.append(abilityEl);
+function startQuiz() {
+  var quizIntroInfo = document.querySelector("#quizBox");
+  quizIntroInfo.setAttribute("class", "hide");
+  questionsDisplay.removeAttribute("class");
+  timerId = setInterval(countdown, 1000);
+  timerDisplay.textContent = time;
+  quizQuestions();
 }
 
-// select all `<div>` elements on the page
-// document.querySelectorAll('div');
-console.log($('div'));
+function countdown() {
+  time--;
+  timerDisplay.textContent = time;
 
-// Alternatively use the `$.each` method
-// $.each(abilities, function(i, ability) {
-//    abilityEl.append("<div>" + ability + "</div>");
-// })
+  if (time <= 0) {
+    quizEnd();
+  }
+}
+
+function quizQuestions() {
+  var currentQuestion = questions[currentQuestionIndex];
+  var questionTitle = document.querySelector("#question-title");
+  questionTitle.textContent = currentQuestion.title;
+  answerOptions.innerHTML = "";
+  currentQuestion.choices.forEach(function(choice, i) {
+    var answerButton = document.createElement("button");
+    answerButton.setAttribute("class", "answer-options button");
+    answerButton.setAttribute("value", choice);
+    answerButton.textContent = i + 1 + ". " + choice;
+    answerButton.onclick = questionClick;
+    answerOptions.appendChild(answerButton);
+  });
+}
+
+function questionClick() {
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    time -= 10;
+
+  if (time < 0) {
+      time = 0;
+    }
+    timerDisplay.textContent = time;
+    rightWrong.textContent = "Wrong!";
+    rightWrong.style.color = "gray";
+    rightWrong.style.fontSize = "22px";
+  } else { 
+    time += 10;
+    timerDisplay.textContent = time;
+    rightWrong.textContent = "Correct!";
+    rightWrong.style.color = "gray";
+    rightWrong.style.fontSize = "22px";
+  }
+
+  rightWrong.setAttribute("class", "right-wrong");
+  setTimeout(function() {
+    rightWrong.setAttribute("class", "right-wrong hide");
+  }, 1000);
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    quizQuestions();
+  }
+}
+
+function quizEnd() {
+  clearInterval(timerId);
+  var quizFinished = document.querySelector("#quiz-finished");
+  quizFinished.removeAttribute("class");
+  var finalScoreDisplay = document.querySelector("#final-score");
+  finalScoreDisplay.textContent = time;
+  questionsDisplay.setAttribute("class", "hide");
+}
+
+function saveHighscore() {
+  var initials = userInitialsInput.value.trim();
+  if (initials !== "") {
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    window.location.href = "highscores.html";
+  }
+}
+
+function pressEnter(event) {
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+
+userInitialsInput.onkeyup = pressEnter;
+submitButton.onclick = saveHighscore;
+startButton.onclick = startQuiz;
+
+
+
+
+
+
+
+function printHighscores() {
+  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+  highscores.sort(function(a, b) {
+    return b.score - a.score;
+  });
+
+  highscores.forEach(function(score) {
+    var userScores = document.createElement("li");
+    userScores.textContent = score.initials + " - " + score.score + " 🏅";
+
+    var highscoresList = document.getElementById("highscores-list");
+    highscoresList.appendChild(userScores);
+  });
+}
+
+function clearHighscores() {
+  window.localStorage.removeItem("highscores");
+  window.location.reload();
+}
+document.getElementById("clear").onclick = clearHighscores;
+
+printHighscores();
